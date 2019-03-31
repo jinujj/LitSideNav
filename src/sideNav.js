ca932949813f4950858d6892245a7a6a0fcd8f5e
jinus303 <jinu.john.j@gmail.com>
2019-03-30 22:47:46 -0700

    Added Orientation option for side navigation.

--- src/sideNav.js
+++ src/sideNav.js
@@ -9,65 +9,117 @@ export default class SideNav extends LitElement {
                 display: flex;
                 flex-direction: column;
                 text-align: left;
-                height: 100%; 
-                width: 0; 
                 position: fixed; 
                 z-index: 1; 
-                top: 0; 
-                right: 0;
                 background-color: #ccc; 
                 overflow-x: hidden; 
                 padding-top: 60px; 
                 transition: 0.5s; 
-          }
-          a {
-            font-size: 20px;
-            line-height: 30px;
-            border: solid 1px grey;
-            transition: 0.3s;
-          }`;
+            }
+            .right {
+                top: 0; 
+                right: 0;
+                height: 100%; 
+                width: 0; 
+            }
+            .left {
+                top: 0; 
+                left: 0;
+                height: 100%; 
+                width: 0; 
+            }
+            .top {
+                top: 0; 
+                left: 0;
+                height: 0; 
+                padding-top: 0; 
+                width: 100%; 
+                text-align: center;
+            }
+            .bottom {
+                bottom: 0; 
+                left: 0;
+                height: 0; 
+                padding-top: 0; 
+                width: 100%; 
+                text-align: center;
+            }
+            a {
+                font-size: 20px;
+                line-height: 30px;
+                border: solid 1px grey;
+                transition: 0.3s;
+            }`;
     }
 
     static get properties() {
         return {
             navigation: { type: Array },
+            direction: {type: String}
         };
     }
 
     constructor() {
         super();
+        this.direction = "right";
         this._attachListener();
     }
 
+    /**
+     * Attach the custom event listeners for rendering the side navigation.
+     */
     _attachListener() {
         // Added the custom Event Listener
         this.addEventListener(`side-nav-toggle`, (data) => {
-            const sideNav = this.shadowRoot.querySelector(`.sidenav`);
-            if (!sideNav.style.width) {
-                sideNav.style.width = `300px`;
-            } else {
-                sideNav.style.width = ``;
-            }
+            this._renderNavigation();
         });
     }
 
+    /**
+     * Render the side naviagtion based on the orientation selection.
+     */
+    _renderNavigation() {
+        const sideNav = this.shadowRoot.querySelector(`.sidenav`);
+        switch(this.direction) {
+            case "left":
+            case "right" :
+                sideNav.style.height = `100%`;
+                if (!sideNav.style.width) {
+                    sideNav.style.width = `300px`;
+                } else {
+                    sideNav.style.width = ``;
+                }
+                break;
+            case "top": 
+            case "bottom": 
+                sideNav.style.width = `100%`;
+                if (!sideNav.style.height) {
+                    sideNav.style.height = `200px`;
+                } else {
+                    sideNav.style.height = ``;
+                }
+        }
+    }
+
+    /**
+     * Handle the close button in the side naviagation.
+     */
     _handleClose() {
         this.dispatchEvent(new CustomEvent(`side-nav-toggle`, {
             detail: { click: `CLICK` },
             bubbles: true
-        })
-        );
+        }));
     }
 
     render() {
         if (this.navigation && this.navigation.length) {
             return html`
-            <div class="sidenav">
-                <a href="#" class="close" @click=${this._handleClose}>&times;</a>
-                ${this.navigation.map((item) =>
-                html`<a href="#">${item}</a>`)}  
-            </div>
-        `;
+                <div class="sidenav ${this.direction}">
+                    <a href="#" class="close" @click=${this._handleClose}>&times;</a>
+                    ${this.navigation.map((item) =>
+                        html`<a href="#">${item}</a>`)}  
+                </div>
+            `;
         } else {
             return html``;
         }
